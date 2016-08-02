a1072f4456ce2a96acfab44f8111bd886adfe73d
RexCorpDev <rexcorpfx@gmail.com>
2016-08-01 21:39:24 -0700

    mods crud routes completed

--- tools/db/mod.js
+++ tools/db/mod.js
@@ -5,6 +5,7 @@ const modSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  model: { type: String, required: true },
   images: [{
     type: String,
   }],
@@ -20,14 +21,35 @@ const modSchema = new mongoose.Schema({
   options: {
     popularity: { type: Number },
     rating: { type: Number },
-    dateAdded: { type: Date.now },
+    dateAdded: { type: Date, default: Date.now },
     style: [{ type: String }],
     colors: [{ type: String }],
     sizes: [{ type: String }],
   },
 });
 
+modSchema.statics.removeOne = (id, cb) => {
+  if (!id) return cb({ Error: 'Missing required Id to Delete Mod.' });
 
+  return Mod.findByIdAndRemove(id, (err1) => {
+    if (err1) return cb({ Error: 'Could not delete Mod. Check id.' });
+    return Mod.find({}, (err2, dbData) => {
+      if (err2) return cb({ Error: 'Could not retrieve DB Mods.' });
+      return cb(null, dbData);
+    });
+  });
+}
 
+modSchema.statics.updateMod = (id, body, cb) => {
+  if (!id) return cb({ Error: 'Missing required Id to Delete Mod.' });
+
+  return Mod.findByIdAndUpdate(id, body, 'new', (err1, newMod) => {
+    if (err1) return cb({ Error: 'Could not delete Mod. Check id.' });
+    return Mod.find({}, (err2, dbData) => {
+      if (err2) return cb({ Error: 'Could not retrieve DB Mods.' });
+      return cb(null, dbData);
+    });
+  });
+}
 const Mod = mongoose.model('Mod', modSchema);
 export default Mod
