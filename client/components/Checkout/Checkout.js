9cbeae108fd4322451f8dfe7be617e1c4ef2d970
RexCorpDev <rexcorpfx@gmail.com>
2016-08-04 10:01:58 -0700

    building sidebar checkout view

--- client/components/Checkout/Checkout.js
+++ client/components/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
+import CartTable from '../Cart/'
 // TODO Implement Guest checkout option
 
 // TODO Order Progress Bar
@@ -51,9 +52,25 @@ export default class Checkout extends Component {
     <div className="sidebar_content">
       <div className="order-summary">
         <h2>Order Summary</h2>
-        <div className="order-summary-products"></div>
-        <div className="order-summary-discount"></div>
-        <div className="order-summary-total"></div>
+        <div className="order-summary-sections">
+          <div className="order-summary-products">
+            <div className="order-summary-section-content">
+              <table>
+                <caption>Checkout Cart</caption>
+                <thead>
+                  <tr className="visually-hidden">
+                    <th>Image</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+          </div>
+          <div className="order-summary-discount"></div>
+          <div className="order-summary-total"></div>
+        </div>
       </div>
     </div>
   </div>
