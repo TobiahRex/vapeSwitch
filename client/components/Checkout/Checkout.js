fc2c6742285fbbe171db479ce0e3ea02c7a99031
RexCorpDev <rexcorpfx@gmail.com>
2016-08-04 09:54:31 -0700

    built breadcrumb banner

--- client/components/Checkout/Checkout.js
+++ client/components/Checkout/Checkout.js
@@ -12,31 +12,51 @@ import { Link } from 'react-router'
 export default class Checkout extends Component {
   render() {
     return (
-      <div id="checkout-main">
-        <div id="main-header">
-
-        </div>
-        <div id="main-content">
-
-        </div>
-        <div id="main-footer">
-
-        </div>
-      </div>
     )
   }
 }
-<ul class="breadcrumb ">
-    <li class="breadcrumb__item breadcrumb__item--completed">
-      <Link class="breadcrumb__link" to="cart">Cart</Link>
-    </li>
-    <li class="breadcrumb__item breadcrumb__item--current">
-      Customer information
-    </li>
-    <li class="breadcrumb__item breadcrumb__item--blank">
-      Shipping method
-    </li>
-    <li class="breadcrumb__item breadcrumb__item--blank">
-      Payment method
-    </li>
-</ul>
+<div className="wrapper">
+  <div className="main">
+    <div id="checkout-main">
+      <div id="main-header">
+        <ul className="list-unstyled list-inline">
+          <li className="breadcrumb__item breadcrumb__item--completed">
+            <Link className="breadcrumb__link" to="cart">Cart</Link>
+          </li>
+          <li className='text-info breadcrumb-arrow'> > </li>
+          <li className="breadcrumb__item breadcrumb__item--current">
+            Customer information >
+          </li>
+          <li className='text-info breadcrumb-arrow'> > </li>
+          <li className="breadcrumb__item breadcrumb__item--blank">
+            Shipping method >
+          </li>
+          <li className='text-info breadcrumb-arrow'> > </li>
+          <li className="breadcrumb__item breadcrumb__item--blank">
+            Payment method
+          </li>
+        </ul>
+      </div>
+      <div id="main-content">
+
+      </div>
+      <div id="main-footer">
+
+      </div>
+    </div>
+  </div>
+
+  <div className="sidebar">
+    <div className="sidebar_header"></div>
+    <div className="sidebar_content">
+      <div className="order-summary">
+        <h2>Order Summary</h2>
+        <div className="order-summary-products"></div>
+        <div className="order-summary-discount"></div>
+        <div className="order-summary-total"></div>
+      </div>
+    </div>
+  </div>
+
+
+</div>
