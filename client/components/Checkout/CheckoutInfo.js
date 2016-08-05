b25fe664fdc4b1371fdcc91b224e59833cc1b87e
RexCorpDev <rexcorpfx@gmail.com>
2016-08-04 22:10:00 -0700

    built shipping elements

--- client/components/Checkout/CheckoutInfo.js
+++ client/components/Checkout/CheckoutInfo.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
 import CheckoutBreadcrumb from './breadcrumb'
+import CountrySelector from './countrySelector'
 
 export default class CheckoutInfo extends Component {
   render() {
@@ -21,12 +23,35 @@ export default class CheckoutInfo extends Component {
             </div>
             <div className="customer-info-container">
               <h3 className="customer-info-title">Customer Information</h3>
-              <input type="text" className="customer-info-email form-control"/>
+              <input type="email" className="customer-info customer-info-email form-control" placeholder="Email" required/>
+              <h5>Already have an account with us? <span className="text-success" id="custom-info-login-redirect"> Log in</span> </h5>
+
+              <h3 className="customer-info-title">Shipping Address</h3>
+              <div className="form-group">
+                <input type="text" className="customer-info custom-info-firstname form-control" placeholder="First name" required/>
+
+                <input type="text" className="customer-info custom-info-lastname form-control" placeholder="Last name" required/>
+
+                <input type="text" className="customer-info custom-info-company form-control" placeholder="Company (optional)"/>
+
+                <input type="text" className="customer-info custom-info-address form-control" placeholder="Address" required/>
+
+                <input type="text" className="customer-info custom-info-apt form-control" placeholder="Apt, suite, etc. (optional)"/>
+
+                <input type="text" className="customer-info custom-info-city form-control" placeholder="City" require/>
+
+                <CountrySelector />
+
+                <input type="text" className="customer-info custom-info-state form-control" placeholder="State" required/>
+
+                <input type="text" className="customer-info custom-info-zip form-control" placeholder="Zip code" required/>
+
+                <input type="text" className="customer-info customer-info-phone form-control" placeholder="Phone (optional)"/>
+              </div>
             </div>
           </div>
           <div id="main-footer">
           </div>
-
         </div>
       </div>
     )
